45a177c94530c28a0235286daed466f69e28e9cf
Abhinavkar <karabhinav24@gmail.com>
2023-05-29 15:22:23 +0530

    Added-javascript

---
+++ Day-1 29-5-2023/Introduction/index.js
@@ -0,0 +1,29 @@
+
+// function Sum(a,b){
+//     return a+b;
+
+// }
+// console.log(Sum(10,20));
+// let a = [10,20,30,40]
+// let b={
+//     a:10,
+//     b:10
+// }
+// let c = [
+//     {
+//         a:10,
+//         b:20
+//     },
+//     {
+//         a:10,
+//         b:20
+//     }
+// ]
+// console.log(a);
+// a.push(50);
+// console.log(a);
+a = [1,2,3,4,5,6,7,8,9,10]
+for(let i =0 ; i<a.length ; i++){
+    a[i]%2==0?console.log(a[i]):null;
+}
+console.log("hello");
