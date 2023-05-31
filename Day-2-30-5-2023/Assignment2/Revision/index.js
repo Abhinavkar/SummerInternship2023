b3aa0bb618ff2787c2555778690fafb2cb234855
Abhinavkar <karabhinav24@gmail.com>
2023-05-31 10:41:09 +0530

    by

---
+++ Day-2-30-5-2023/Assignment2/Revision/index.js
@@ -0,0 +1,9 @@
+const tr = document.createElement('tr');
+const td = document.createElement('td');
+const text = document.innerText = 'Hello World';
+btn=document.getElementById('btn');
+btn1=document.getElementById('btn1');
+btn.addEventListener("click",()=>{
+    document
+
+});
